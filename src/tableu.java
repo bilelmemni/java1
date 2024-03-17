5a6215ec3a47d9090eefa6a469d5cae3606140bf
hero <bilelbenammarmemni@gmail.com>
2024-03-17 22:37:34 +0100

    seconde_commit

--- src/tableu.java
+++ src/tableu.java
@@ -4,14 +4,16 @@ public class tableu {
         int[] tabint = { 66, 13, 57 };
         int[][] tabint2 = { { 15, 20, 19, 18 }, { 22, 33, 44, 55 } };
         String[] tabstring = { "java", "php", "angular" };
-        for (int i = 0; i < tabstring.length; i++) {
-            System.out.println("tab" + i + "=" + tabstring[i]);
-            System.out.println(tabint[i]);
-
-        }
-        for (String i : tabstring) {
-            System.out.println(i);
-        }
+        
+          for (int i = 0; i < tabstring.length; i++) {
+          System.out.println("tab" + i + "=" + tabstring[i]);
+          System.out.println(tabint[i]);
+          
+          }
+          for (String i : tabstring) {
+          System.out.println(i);
+          }
+         
         System.out.println(tabint2.length);
         System.out.println(tabint2[0].length);
         for (int i = 0; i < tabint2.length; i++) {
